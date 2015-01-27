3a1efef3edab04d136262798237424c095de865d
smart <wupeng_xg@163.com>
2015-01-27 16:55:50 +0800

    edit coding style

--- eventQueue.js
+++ eventQueue.js
@@ -1,4 +1,7 @@
 (function(win){
+	var trimLeft = /(\{|\(|\[|\]|\)|\})(\u3000|\s|\t)+/;		
+	var space = /(\u3000|\s|\t)*\n(\u3000|\s|\t)*/g;		
+	var whitespace = /(^\u3000|\s|\t)(\u3000|\s|\t)*/gi;
 	var Event = {
 		resizeTimer : null,
 		map : {},
@@ -16,10 +19,7 @@
 				queue[i](e);
 			}
 		},
-		findFun : function(fn,queue){
-			var trimLeft = /(\{|\(|\[|\]|\)|\})(\u3000|\s|\t)+/;		
-			var space = /(\u3000|\s|\t)*\n(\u3000|\s|\t)*/g;		
-			var whitespace = /(^\u3000|\s|\t)(\u3000|\s|\t)*/gi;
+		findFun : function(fn,queue){			
 			for(var i in queue){				
 				var fn = fn.toString().replace(space,'').replace(whitespace,' ').replace(trimLeft,'$1');
 				var qu = queue[i].toString().replace(space,'').replace(whitespace,' ').replace(trimLeft,'$1');				
@@ -34,79 +34,69 @@
 				if(index) queue.splice(index,1);
 			}
 		},
-		_addEventListener : function(el,type,fn,useCapture){
+		_addEventListener : function(el,type,fn,useCapture){			
 			return Event.addQueue(el,type,fn,useCapture,function(){					
-				el.addEventListener(type,function(e){					
+				el.addEventListener(type,function(e){				
 					Event._fire(type,el,e);					
 				},useCapture);
 			})
 		},
-		_attachEvent : function(el,type,fn,useCapture){			
+		_attachEvent : function(el,type,fn,useCapture){
 			return Event.addQueue(el,type,fn,useCapture,function(){			
 				el.attachEvent('on' + type,function(e){					
 					Event._fire(type,el,e);
 				});
 			})
 		},
-		add : function(el,type,fn,useCapture){			
-			if(document.addEventListener){
-				return function(el,type,fn,useCap){
-					var useCap = useCap ? useCap : false;
-					if(type == 'resize'){
-						return Event.addQueue(el,type,fn,useCap,function(){
-							el.addEventListener(type,function(e){								
-								if(Event.resizeTimer) clearTimeout(Event.resizeTimer);
-								Event.resizeTimer = setTimeout(function(){									
-									Event._fire(type,el,e);
-								},50);								
-							},useCap);
-						})
-						return true;
-					}					
-					Event._addEventListener(el,type,fn,useCap);
+		add : function(){
+			return document.addEventListener ? function(el,type,fn,useCapture){				
+				var useCap = useCapture ? useCapture : false;
+				if(type == 'resize'){
+					return Event.addQueue(el,type,fn,useCap,function(){
+						el.addEventListener(type,function(e){													
+							if(Event.resizeTimer) clearTimeout(Event.resizeTimer);
+							Event.resizeTimer = setTimeout(function(){									
+								Event._fire(type,el,e);
+							},50);								
+						},useCap);
+					})
+					return true;
 				}				
-			}
-			else{
-				return function(el,type,fn,useCap){
-					var useCap = useCap ? useCap : false;
-					if(type == 'resize'){//解决resize事件多次执行的bug						
-		                return Event.addQueue(el,type,fn,useCap,function(){
-							el.attachEvent('on' + type,function(e){
-								if(Event.resizeTimer) clearTimeout(Event.resizeTimer);
-								Event.resizeTimer = setTimeout(function(){
-									Event._fire(type,el,e);
-								},50);
-							});
-						})           
-		           		return true;
-		       		}		       		
-		       		Event._attachEvent(el,type,fn,useCap);
-				}				 
-			}	
-		},
+				Event._addEventListener(el,type,fn,useCap);
+			} : function(el,type,fn,useCapture){
+				var useCap = useCapture ? useCapture : false;
+				if(type == 'resize'){//解决resize事件多次执行的bug						
+	                return Event.addQueue(el,type,fn,useCap,function(){
+						el.attachEvent('on' + type,function(e){							
+							if(Event.resizeTimer) clearTimeout(Event.resizeTimer);
+							Event.resizeTimer = setTimeout(function(){
+								Event._fire(type,el,e);
+							},50);
+						});
+					})           
+	           		return true;
+	       		}		       		
+	       		Event._attachEvent(el,type,fn,useCap);
+			}			
+		}(),
 		remove : function(el,type,fn,useCapture){			
-			useCapture = useCapture ? useCapture : false;
-			if(document.attachEvent){
-				return function(el,type,fn,useCapture){
-					return function(el,type,fn,useCapture){
-						Event.removeFn(el,type,fn,useCapture,function(fn){
-							el.detachEvent('on' + type,fn);
-						});
-					}(el,type,fn)
-				}				
+			return document.attachEvent ? function(el,type,fn){
+				var useCap = useCap ? useCap : false;
+				return function(el,type,fn,useCap){
+					Event.removeFn(el,type,fn,useCap,function(fn){
+						el.detachEvent('on' + type,fn);
+					});
+				}(el,type,fn)
+			} : function(el,type,fn){
+				var useCap = useCap ? useCap : false;
+				return function(el,type,fn,useCap){
+					Event.removeFn(el,type,fn,useCap,function(fn){
+						el.removeEventListener(type,fn,useCap);
+					});
+				}(el,type,fn)
 			}
-			else
-			{
-				return function(el,type,fn){
-					return function(el,type,fn,useCapture){
-						Event.removeFn(el,type,fn,useCapture,function(fn){
-							el.removeEventListener(type,fn,useCapture);
-						});
-					}(el,type,fn)
-				}				
-			}
-		}
+		}()
 	}
-	win.add = Event.add();
-	win.remove = Event.remove();	
+	win.add = Event.add;	
+	win.remove = Event.remove;	
 })(window)
